0782165639b76cb0ef963f6679bac46ca1ea0e27
lowenskitmb <lowenski.matos@outlook.com>
2024-06-07 06:24:16 -0400

    primer commit

---
+++ api/Program.cs
@@ -0,0 +1,30 @@
+using api.Data;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddDbContext<ApplicationDBContext>(options => {
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.MapControllers();
+
+app.Run();
+//docker run -e 'ACCEPT_EULA=Y' -e 'SA_PASSWORD=04030404AWAWAWsq4#' -p 1433:1433 --name sqlserver -d mcr.microsoft.com/mssql/server:latest
